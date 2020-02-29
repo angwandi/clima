28a14f4441dd7f2f3586f05052809e03ffee70f6
Innocent Dema <31923567+angwandi@users.noreply.github.com>
2020-02-29 12:20:56 +0300

    Initial commit

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,42 @@
+import 'package:clima/services/location.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  //only get called once
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
+  void getData() async {
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
+//get called every times when a widget changes
+  @override
+  Widget build(BuildContext context) {
+    getData();
+    return Scaffold();
+  }
+}
---
+++ scratch.dart
@@ -0,0 +1,29 @@
+void main() {
+  performTasks();
+}
+
+void performTasks() async {
+  task1();
+  String taskResult = await task2();
+  task3(taskResult);
+}
+
+void task1() {
+  String result = 'task 1 data';
+  print('Task 1 complete');
+}
+
+Future<String> task2() async {
+  Duration duration = Duration(seconds: 3);
+  String result;
+  await Future.delayed(duration, () {
+    result = 'task 2 data';
+    print('Task 2 complete');
+  });
+  return result;
+}
+
+void task3(String task2data) {
+  String result = 'task 3 data';
+  print('Task 3 complete with $task2data');
+}
